39d28b799b2e9127dc9f1450c8a84f2962f2206e
htsonca1991 <36769341+htsonca1991@users.noreply.github.com>
2018-11-22 17:13:45 +0700

    Update Test 1

--- update.bat
+++ update.bat
@@ -1,1 +1,1 @@
-bash -c "cd D:/IOS/73; ./update.sh;"
+bash -c "cd D:/IOS/7.3; ./update.sh;"
--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 bzip2 -c9k ./Packages > ./Packages.bz2
-printf "Origin: KidsAuto IOS Repo73\nLabel: KidsAuto IOS Repo73\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitecture: iphoneos-arm\nComponents: main\nDescription: KidsAuto for IOS Model 9.2.1 - 10.3.3\nMD5Sum:\n "$(cat ./Packages | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages)" Packages\n "$(cat ./Packages.bz2 | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages.bz2)" Packages.bz2\n" >Release;
+printf "Origin: KidsAuto IOS Repo 7.3\nLabel: KidsAuto IOS Repo 7.3\nSuite: stable\nVersion: 1.0\nCodename: ios\nArchitecture: iphoneos-arm\nComponents: main\nDescription: Test KidsAuto for IOS Model 9.2.1 - 10.3.3\nMD5Sum:\n "$(cat ./Packages | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages)" Packages\n "$(cat ./Packages.bz2 | md5 | cut -d ' ' -f 1)" "$(stat -f%z ./Packages.bz2)" Packages.bz2\n" >Release;
 exit 0
